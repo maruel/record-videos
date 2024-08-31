480962d4b932ebb12a8cd82b9faea9a86ae4c0ce
Marc-Antoine Ruel <maruel@gmail.com>
2024-08-31 08:30:32 -0400

    Move server code into server.go

--- main.go
+++ main.go
@@ -11,31 +11,22 @@ import (
 	"bufio"
 	"bytes"
 	"context"
-	_ "embed"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"html/template"
 	"io"
-	"io/fs"
-	"iter"
 	"log/slog"
 	"math"
-	"mime/multipart"
-	"net"
 	"net/http"
-	"net/textproto"
-	"net/url"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path/filepath"
 	"runtime"
-	"sort"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
@@ -44,15 +35,6 @@ import (
 	"github.com/mattn/go-isatty"
 	"golang.org/x/sync/errgroup"
 )
-
-//go:embed html/videos.html
-var videosHTML []byte
-
-//go:embed html/list.html
-var listHTML []byte
-
-// Injected data to speed up page load, versus having to do an API call.
-var dataTmpl = template.Must(template.New("").Parse("<script>'use strict';const data = {{.}};</script>"))
 
 // filter is a filter supported by libavfilter.
 //
@@ -602,211 +584,6 @@ func processMotion(ctx context.Context, root string, ch <-chan motionEvent, onEv
 			}
 		}
 	}
-}
-
-// broadcastFrames broadcast MPJPEG frames to listeners.
-type broadcastFrames struct {
-	mu sync.Mutex
-	l  []chan []byte
-}
-
-// listen reads ffmpeg's mpjpeg mime stream, decodes it, then send it to
-// readers.
-func (b *broadcastFrames) listen(ctx context.Context, r io.Reader) {
-	mr := multipart.NewReader(r, "ffmpeg")
-	for i := 0; ctx.Err() == nil; i++ {
-		p, err := mr.NextPart()
-		if err == io.EOF {
-			return
-		}
-		if err != nil {
-			return
-		}
-		frame, err := io.ReadAll(p)
-		if err != nil {
-			return
-		}
-		// First frame read.
-		if i == 0 {
-			slog.Info("ready")
-		}
-		//slog.Debug("mjpeg", "i", i, "b", len(frame))
-		// Expected p.Header:
-		//	Content-type: image/jpeg
-		//	Content-length: 1234
-		b.mu.Lock()
-		l := make([]chan []byte, len(b.l))
-		copy(l, b.l)
-		b.mu.Unlock()
-		for _, x := range l {
-			select {
-			case x <- frame:
-			default:
-			}
-		}
-	}
-}
-
-func (b *broadcastFrames) relay(ctx context.Context) iter.Seq[[]byte] {
-	ch := make(chan []byte, 1)
-	b.mu.Lock()
-	b.l = append(b.l, ch)
-	b.mu.Unlock()
-	return func(yield func([]byte) bool) {
-		defer func() {
-			b.mu.Lock()
-			for i := range b.l {
-				if b.l[i] == ch {
-					copy(b.l[i:], b.l[i+1:])
-					b.l = b.l[:len(b.l)-1]
-					break
-				}
-			}
-			b.mu.Unlock()
-		}()
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case frame := <-ch:
-				if !yield(frame) {
-					return
-				}
-			}
-		}
-	}
-}
-
-func startServer(ctx context.Context, addr string, r io.Reader, root string) error {
-	m := http.ServeMux{}
-	bf := &broadcastFrames{}
-	go bf.listen(ctx, r)
-
-	// MJPEG stream
-	m.HandleFunc("GET /mjpeg", func(w http.ResponseWriter, req *http.Request) {
-		start := time.Now()
-		slog.Info("http", "remote", req.RemoteAddr)
-		mw := multipart.NewWriter(w)
-		defer mw.Close()
-		h := w.Header()
-		h.Set("Content-Type", "multipart/x-mixed-replace;boundary="+mw.Boundary())
-		//h.Set("Content-Type", "multipart/x-mixed-replace;boundary=FRAME")
-		h.Set("Connection", "close")
-		h.Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-		h.Set("Pragma", "no-cache")
-		h.Set("Expires", "0")
-		i := 0
-		for frame := range bf.relay(req.Context()) {
-			slog.Debug("http", "remote", req.RemoteAddr, "i", i, "b", len(frame))
-			fw, err := mw.CreatePart(textproto.MIMEHeader{
-				"Content-Type":   []string{"image/jpeg"},
-				"Content-Length": []string{strconv.Itoa(len(frame))},
-			})
-			if err != nil {
-				break
-			}
-			if _, err := fw.Write(frame); err != nil {
-				break
-			}
-			i++
-		}
-		slog.Info("http", "remote", req.RemoteAddr, "done", true, "d", time.Since(start).Round(100*time.Millisecond))
-	})
-
-	// Video serving.
-	m.HandleFunc("GET /raw/", func(w http.ResponseWriter, req *http.Request) {
-		path, err2 := url.QueryUnescape(req.URL.Path)
-		if err2 != nil {
-			slog.Error("http", "path", req.URL.Path)
-			http.Error(w, "Invalid path", 404)
-			return
-		}
-		f := path[len("/raw/"):]
-		// Limit to not path, only .m3u8 and .ts.
-		if strings.Contains(f, "/") || strings.Contains(f, "\\") || strings.Contains(f, "..") || (!strings.HasSuffix(f, ".m3u8") && !strings.HasSuffix(f, ".ts")) {
-			slog.Error("http", "path", req.URL.Path)
-			http.Error(w, "Invalid path", 404)
-			return
-		}
-
-		// Cache for a long time, the exception is m3u8 since it could be a live
-		// playlist.
-		if h := w.Header(); strings.HasSuffix(f, ".m3u8") {
-			h.Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-			h.Set("Pragma", "no-cache")
-			h.Set("Expires", "0")
-		} else {
-			h.Set("Cache-Control", "public, max-age=86400")
-		}
-		http.ServeFile(w, req, filepath.Join(root, f))
-	})
-
-	// HTML
-	m.HandleFunc("GET /list", func(w http.ResponseWriter, req *http.Request) {
-		var files []string
-		offset := len(root) + 1
-		_ = filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-			if !d.IsDir() && strings.HasSuffix(path, ".m3u8") || strings.HasSuffix(path, ".ts") {
-				files = append(files, path[offset:])
-			}
-			return nil
-		})
-		sort.Strings(files)
-		h := w.Header()
-		h.Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-		h.Set("Pragma", "no-cache")
-		h.Set("Expires", "0")
-		h.Set("Content-Type", "text/html; charset=utf-8")
-		if _, err2 := w.Write(listHTML); err2 != nil {
-			return
-		}
-		_ = dataTmpl.Execute(w, map[string]any{"files": files})
-	})
-	m.HandleFunc("GET /videos", func(w http.ResponseWriter, req *http.Request) {
-		var files []string
-		offset := len(root) + 1
-		_ = filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-			if !d.IsDir() && strings.HasSuffix(path, ".m3u8") {
-				files = append(files, path[offset:])
-			}
-			return nil
-		})
-		sort.Strings(files)
-		h := w.Header()
-		h.Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-		h.Set("Pragma", "no-cache")
-		h.Set("Expires", "0")
-		h.Set("Content-Type", "text/html; charset=utf-8")
-		if _, err2 := w.Write(videosHTML); err2 != nil {
-			return
-		}
-		_ = dataTmpl.Execute(w, map[string]any{"files": files})
-	})
-
-	m.HandleFunc("GET /", func(w http.ResponseWriter, req *http.Request) {
-		if req.URL.Path == "/" {
-			http.Redirect(w, req, "videos", http.StatusFound)
-			return
-		}
-		slog.Error("http", "path", req.URL.Path)
-		http.Error(w, "Not found", http.StatusNotFound)
-	})
-	s := http.Server{
-		Handler:      &m,
-		BaseContext:  func(net.Listener) context.Context { return ctx },
-		ReadTimeout:  10. * time.Second,
-		WriteTimeout: 366 * 24 * time.Hour,
-		IdleTimeout:  10. * time.Second,
-	}
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		return err
-	}
-	slog.Info("http", "addr", l.Addr())
-	go s.Serve(l)
-	// TODO: clean shutdown.
-	//s.Shutdown(context.Background())
-	return nil
 }
 
 // buildFFMPEGCmd builds the command line to exec ffmpeg.
